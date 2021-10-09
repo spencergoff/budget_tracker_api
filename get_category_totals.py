23b15bfa2bef66eb2c1379b04a0dc06ab15761fc
spencergoff <spencer.goff95@yahoo.com>
2021-10-09 11:17:29 -0700

    dev

--- get_category_totals.py
+++ get_category_totals.py
@@ -1,9 +1,5 @@
 import os
-import flask
 
-app = flask.Flask(__name__)
-
-@app.route('/')
 def hello_world(event, context):
     body = 'Hello, world!'
     return {
@@ -11,16 +7,3 @@ def hello_world(event, context):
         'headers': {'Content-Type': 'application/json'},
         'body': body
     }
-
-def running_in_aws_lambda():
-    if os.environ.get('AWS_EXECUTION_ENV') == None:
-        return False
-    else:
-        return True
-
-if __name__ == '__main__':
-    if not running_in_aws_lambda():
-        print('Not running in Lambda')
-        app.run(host='0.0.0.0', port=5000)
-    else:
-        print('Running in Lambda')
