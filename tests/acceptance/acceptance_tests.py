c706dc8a3a88ccd8c779327d94cc024c91a769cc
spencergoff <spencer.goff95@yahoo.com>
2021-11-03 08:50:56 -0700

    test

--- tests/acceptance/acceptance_tests.py
+++ tests/acceptance/acceptance_tests.py
@@ -5,8 +5,8 @@ import requests
 
 class TestClass:
     def test_one(self):
-        qa_endpoint = os.environ["qa_endpoint"]
-        print(f'qa endpoint: {qa_endpoint}')
+        qa_endpoint = os.environ["qa_endpoint"].strip('"')
+        print(f'qa_endpoint: {qa_endpoint}')
         qa_endpoint_content = requests.get(qa_endpoint)
         print(f'qa_endpoint_content.text: {qa_endpoint_content.text}')
         print(f'qa_endpoint_content.status_code: {qa_endpoint_content.status_code}')
