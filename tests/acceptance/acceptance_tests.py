ff6e2fb7e12c844da57b9f6147b1de4066f9670a
spencergoff <spencer.goff95@yahoo.com>
2021-11-03 08:22:13 -0700

    test

--- tests/acceptance/acceptance_tests.py
+++ tests/acceptance/acceptance_tests.py
@@ -4,6 +4,6 @@ import pytest
 
 class TestClass:
     def test_one(self):
-        print(f'qa endpoint: {os.environ['qa_endpoint']}')
+        print(f'qa endpoint: {os.environ["qa_endpoint"]}')
         x = "this"
         assert "h" in x
