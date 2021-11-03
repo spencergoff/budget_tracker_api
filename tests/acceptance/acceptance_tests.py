bec83c6a9d57229a38dc9d01d7a908432c8c39f2
spencergoff <spencer.goff95@yahoo.com>
2021-11-03 08:42:01 -0700

    test

--- tests/acceptance/acceptance_tests.py
+++ tests/acceptance/acceptance_tests.py
@@ -1,9 +1,13 @@
 import os
 import sys
 import pytest
+import requests
 
 class TestClass:
     def test_one(self):
-        print(f'qa endpoint: {os.environ["qa_endpoint"]}')
-        x = "this"
-        assert "h" in x
+        qa_endpoint = os.environ["qa_endpoint"]
+        print(f'qa endpoint: {qa_endpoint}')
+        qa_endpoint_content = requests.get(qa_endpoint)
+        print(f'qa_endpoint_content.text: {qa_endpoint_content.text}')
+        print(f'qa_endpoint_content.status_code: {qa_endpoint_content.status_code}')
+        assert qa_endpoint_content.status_code == 2001
