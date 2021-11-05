512f238e0c74b2068f6e25afb1000d4cf1290a8d
spencergoff <spencer.goff95@yahoo.com>
2021-11-05 14:29:49 -0700

    test

---
+++ tests/unit/unit_tests.py
@@ -0,0 +1,19 @@
+# To execute these tests, go to the root directory of this repo and use the command:
+# python -m tests.unit.unit_tests
+
+import unittest
+from get_category_totals import hello_world
+
+class AllTests(unittest.TestCase):
+
+    def test_hello_world(self):
+        expected_return = {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': 'body'
+        }
+        actual_return = hello_world('event', 'context')
+        self.assertEqual(expected_return, actual_return)
+
+if __name__ == '__main__':
+    unittest.main()
