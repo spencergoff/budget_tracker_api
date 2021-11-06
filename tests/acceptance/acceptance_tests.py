c40ce1c812e7d9f72b81bb08b170e9c891c6b184
spencergoff <spencer.goff95@yahoo.com>
2021-11-06 08:11:07 -0700

    test

--- tests/acceptance/acceptance_tests.py
+++ tests/acceptance/acceptance_tests.py
@@ -10,4 +10,4 @@ class TestClass:
         qa_endpoint_content = requests.get(qa_endpoint)
         print(f'qa_endpoint_content.text: {qa_endpoint_content.text}')
         print(f'qa_endpoint_content.status_code: {qa_endpoint_content.status_code}')
-        assert qa_endpoint_content.text == 'Hello, world!
+        assert qa_endpoint_content.text == 'Hello, world!'
