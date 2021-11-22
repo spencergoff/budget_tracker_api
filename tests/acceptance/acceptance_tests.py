5ff969aa8f0f0b1af278b99d04b489c243e2acc4
spencergoff <spencer.goff95@yahoo.com>
2021-11-21 18:16:07 -0800

    dev

--- tests/acceptance/acceptance_tests.py
+++ tests/acceptance/acceptance_tests.py
@@ -6,14 +6,6 @@ from src.get_category_totals import get_expected_weekly_total
 
 class TestClass:
 
-    # def test_health_check(self):
-    #     qa_endpoint = os.environ["qa_endpoint"].strip('"')
-    #     print(f'qa_endpoint: {qa_endpoint}')
-    #     qa_endpoint_content = requests.get(qa_endpoint)
-    #     print(f'qa_endpoint_content.text: {qa_endpoint_content.text}')
-    #     print(f'qa_endpoint_content.status_code: {qa_endpoint_content.status_code}')
-    #     assert qa_endpoint_content.text == 'Hello, world!'
-
     def test_givenUser_whenUserOpensHomePage_thenUsersWeeklyTotalShows(self):
         expected_weekly_total = get_expected_weekly_total()
         assert self.get_home_page_text() == expected_weekly_total
@@ -24,4 +16,4 @@ class TestClass:
         qa_endpoint_content = requests.get(qa_endpoint)
         print(f'qa_endpoint_content.text: {qa_endpoint_content.text}')
         print(f'qa_endpoint_content.status_code: {qa_endpoint_content.status_code}')
-        return qa_endpoint_content
+        return qa_endpoint_content.text
