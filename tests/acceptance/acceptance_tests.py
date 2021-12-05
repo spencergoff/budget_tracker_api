89aff4f8d5f37039f7b5de092fcad10606ed82bd
spencergoff <spencer.goff95@yahoo.com>
2021-12-04 21:01:53 -0800

    dev

--- tests/acceptance/acceptance_tests.py
+++ tests/acceptance/acceptance_tests.py
@@ -8,4 +8,7 @@ class TestClass(Dsl):
 
     def test_home_page_shows_weekly_total(self):
         expected_weekly_total = calculate_weekly_total()
-        assert self.get_home_page_text() == expected_weekly_total
+        expected_home_page_text = f'Total spent this week: {expected_weekly_total}'
+        actual_home_page_text = self.get_home_page_text()
+        print(f'expected_home_page_text: {expected_home_page_text} | actual_home_page_text: {actual_home_page_text}')
+        assert expected_home_page_text == actual_home_page_text
